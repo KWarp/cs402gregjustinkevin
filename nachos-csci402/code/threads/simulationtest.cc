f50538e14438e13e2ba80d2db60fa7c3ff6c2983
WhiteGreene <WhiteGreene@gmail.com>
2010-09-19 10:04:00 +0000

    Variable name fixes to TestCustomer. Implemented TestCook().

--- nachos-csci402/code/threads/simulationtest.cc
+++ nachos-csci402/code/threads/simulationtest.cc
@@ -17,34 +17,98 @@ void TestCustomer()
     ServiceCustomer();
   
   // Give the customers their orders
-  for (int i = 0; i < maxNumOrders; ++i)
+  for (int token = 0; token < count_NumCustomers; ++token)
   {
-    if (ordersNeedingBagging[i] > 0)
+    if (ordersNeedingBagging[token] > 0)
     {
       // If the customer is eat-in.
-      if (Get_CustomerTogoOrEatinFromCustomerID[GET_CustomerIDFromOrderNumber[i]] == 1)
+      if (Get_CustomerTogoOrEatinFromCustomerID[GET_CustomerIDFromOrderNumber[token]] == 1)
       {
         // Pretend we are waiter delivering food and signal the eat-in customer directly.
-        acquire(lock_eatinCustomersWaitingForFood[customerNumberFromorderNumber[i]]);
-        signal(CV_eatinCustomersWaitingForFood[customerNumberFromorderNumber[i]], 
-               lock_eatinCustomersWaitingForFood[customerNumberFromorderNumber[i]]);
-        release(lock_eatinCustomersWaitingForFood[customerNumberFromorderNumber[i]]);
+        Acquire(lock_CustomerSittingFromCustomerID[Get_CustomerIDFromToken[token]]);
+        signal(CV_CustomerSittingFromCustomerID[Get_CustomerIDFromToken[token]],
+               lock_CustomerSittingFromCustomerID[Get_CustomerIDFromToken[token]]);
+        Release(lock_CustomerSittingFromCustomerID[Get_CustomerIDFromToken[token]]);
       }
       else // if customer is togo.
       {
         // Tell the waiting togo customers that order i is ready.
-        acquire(lock_OrderReady);
-          bool_ListOrdersReadyFromToken[i] = 1;
-          broadcast(lock_OrderReady, CV_OrderReady);
-        release(lock_OrderReady);
+        Acquire(lock_OrderReady);
+          bool_ListOrdersReadyFromToken[token] = 1;
+          Broadcast(lock_OrderReady, CV_OrderReady);
+        Release(lock_OrderReady);
       }
     }
   }
 }
 
+void TestWaiter()
+{
+  Acquire(lock_OrWr_BaggedOrders);
+  
+}
+
+void TestCook()
+{
+  // Hire a cook for each type of food.
+  for (int i = 0; i < 5; ++i)
+    hireCook(i);
+
+  // Wait to verify all types of food are being cooked in print statements.
+  Yield(10000);
+
+  // Send all cooks on break.
+  for (int i = 0; i < 5; ++i)
+  {
+    Acquire(lock_MrCk_InventoryLocks[i]);
+    Get_CookOnBreakFromInventoryIndex[i] = TRUE;
+    Release(lock_MrCk_InventoryLocks[i]);
+  }
+
+  // Wait to verify no food is being cooked in print statements.
+  Yield(10000);
+    
+  // Bring all cooks back from break.
+  for (int i = 0; i < 5; ++i)
+  {
+    Acquire(lock_MrCk_InventoryLocks[i]);
+    Get_CookOnBreakFromInventoryIndex[i] = FALSE;
+    Release(lock_MrCk_InventoryLocks[i]);
+  }
+    
+  // Wait to verify all types of food are being cooked in print statements.
+  Yield(10000);
+  
+  // Deplete the inventory for all food.
+  for (int i = 0; i < 5; ++i)
+  {
+    Acquire(lock_MrCk_InventoryLocks[i]);
+    inventoryCount[i] = 0;
+    Release(lock_MrCk_InventoryLocks[i]);
+  }
+    
+  // Wait to verify all types of food are being cooked in print statements.
+  Yield(10000);
+}
+
+void TestOrderTaker()
+{
+
+}
+
+void TestManager()
+{
+
+}
+
 void TestSimulation()
 {
+  // Initialize();
   TestCustomer();
+  TestWaiter();
+  TestCook();
+  TestOrderTaker();
+  TestManager();
 }
 
 #endif
