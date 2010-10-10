a0dc6d10b2f6686f462c72eddb8355e9ed8e5bb9
KWarp11 <KWarp11@64a47c91-1020-2430-03ac-642e8966e14f>
2010-10-10 09:39:56 +0000

    syscall declarations

---
+++ nachos-csci402/code/threads/syscalltest.cc
@@ -0,0 +1,27 @@
+/*
+
+	Syscall test
+
+
+	Test suite for all of the system calls in Project 2
+
+
+*/
+
+#include "simulation.h"
+#include "synch.h"
+#include "simulationtest.h"
+
+
+void TestAcquire()
+{
+
+
+}
+
+
+void testRelease()
+{
+
+
+}
