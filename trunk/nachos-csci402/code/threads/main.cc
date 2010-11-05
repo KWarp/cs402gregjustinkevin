090118ecf4d9135cefb4d661132b8ac6cbe32d40
WhiteGreene@gmail.com <WhiteGreene@gmail.com@64a47c91-1020-2430-03ac-642e8966e14f>
2010-11-05 08:06:09 +0000

    Added -s option to main() so the server gets started. Minor changes to test/testnetsyscalls.c. Changed addrspace to run like it did for project 2 when running in NETWORK.

--- trunk/nachos-csci402/code/threads/main.cc
+++ trunk/nachos-csci402/code/threads/main.cc
@@ -64,6 +64,7 @@ extern void ThreadTest(void), Copy(char *unixFile, char *nachosFile);
 extern void Print(char *file), PerformanceTest(void);
 extern void StartProcess(char *file), ConsoleTest(char *in, char *out);
 extern void MailTest(int networkID);
+extern void Server();
 
 #ifdef CHANGED
   extern void TestSuite(void);
@@ -198,6 +199,11 @@ main(int argc, char **argv)
 						// start up another nachos
             MailTest(atoi(*(argv + 1)));
             argCount = 2;
+  } else if (!strcmp(*argv, "-s")) {  // server
+            Delay(2); 				// delay for 2 seconds
+                              // to give the user time to 
+                              // start up another nachos
+            Server();
         }
 #endif // NETWORK
     }
